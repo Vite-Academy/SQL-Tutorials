a01af3d016ec044e87e73153ededed80432132df
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-08-20 17:11:11 +0500

    done

---
+++ psql/table/create.sql
@@ -0,0 +1,10 @@
+-- https://www.postgresql.org/docs/current/sql-createtable.html
+-- https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-create-table/
+
+-- PostgreSQL create table
+CREATE TABLE [IF NOT EXISTS] [name] (
+   column1 datatype(length) column_contraint,
+   column2 datatype(length) column_contraint,
+   column3 datatype(length) column_contraint,
+   table_constraints
+);
