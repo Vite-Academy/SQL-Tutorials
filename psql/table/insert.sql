66ce6a5311d24af5698862eaf7eebc67483f4c51
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-08-20 18:19:10 +0500

    done

---
+++ psql/table/insert.sql
@@ -0,0 +1,6 @@
+-- https://www.postgresql.org/docs/current/sql-insert.html
+-- https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-insert/
+
+INSERT INTO [name] (column1, column2, …)
+VALUES (value1, value2, …)
+RETURNING *;
