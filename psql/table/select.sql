ca186e22f11d3577ec173f87fd1df337c328bd16
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-08-20 22:27:23 +0500

    done

--- psql/table/select.sql
+++ psql/table/select.sql
@@ -1,6 +1,13 @@
 -- https://www.postgresql.org/docs/current/sql-select.html
 -- https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-select/
 
+-- PostgreSQL select
+-- SELECT
+--    select_list
+-- FROM
+--    table_name;
+
+
 SELECT * FROM [name];
 
 SELECT id FROM [name];
--- psql/table/select_into.sql
+++ psql/table/select_into.sql
@@ -1,3 +1,30 @@
 -- https://www.postgresql.org/docs/current/sql-selectinto.html
 -- https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-select-into/
 
+
+-- PostgreSQL select into
+-- SELECT
+--     select_list
+-- INTO [ TEMPORARY | TEMP | UNLOGGED ] [ TABLE ] new_table_name
+-- FROM
+--     table_name
+-- WHERE
+--     search_condition;
+
+SELECT *
+INTO TABLE users_copy
+FROM users;
+
+SELECT username
+INTO TABLE users_username
+FROM users;
+
+SELECT name, username
+INTO TABLE users_name_username
+FROM users;
+-- ________________________
+
+SELECT username
+INTO TABLE users_username_ali
+FROM users
+WHERE name = 'ali';
